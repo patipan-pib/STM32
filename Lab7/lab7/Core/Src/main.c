5bf52d427c748c31dbc42f881c929809e8e945b4
By_Pib <71693501+patipan-pib@users.noreply.github.com>
2025-02-17 15:32:16 +0700

    up lab7 pwm_moniter

--- Lab7/lab7/Core/Src/main.c
+++ Lab7/lab7/Core/Src/main.c
@@ -30,7 +30,7 @@
 //GPIO_InitTypeDef GPIO_InitStruct = {0};
 uint8_t rxData;
 int redDutyCycle = 0;
-float pwm_r;
+uint8_t pwm_r;
 float pwm_g;
 float pwm_b;
 char tx_message[100];
@@ -114,12 +114,12 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
 //  HAL_UART_Receive_IT(&huart3, &rxData, 1);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, R_DutyCycle);
-  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, G_DutyCycle);
-  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, B_DutyCycle);
+//  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+//  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+//  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+//  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, R_DutyCycle);
+//  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, G_DutyCycle);
+//  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, B_DutyCycle);
 
   snprintf(tx_message, sizeof(tx_message),
   		  "Button (R, G, B) -> 0%, 20%, 40%, 60%, 80%, 100%\r\n"
@@ -134,55 +134,54 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-//	  htim2.Instance -> CCR3 = (1000 - 1 )* dutyCycle;
-//	  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-//	  HAL_Delay(100);
-//	  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_3);
-//	  pwm_r = (GPIOB->IDR & GPIO_PIN_10) >> 10;
+	  htim2.Instance -> CCR3 = (10000 - 1 )* dutyCycle;
+	  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+	  HAL_Delay(100);
+	  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_3);
+	  pwm_r = (GPIOB->IDR & GPIO_PIN_10) >> 10;
     /* USER CODE END WHILE */
 
-
     /* USER CODE BEGIN 3 */
-	  if(HAL_UART_Receive(&huart3, &rxData, 1, 100) == HAL_OK)
-	     {
-		  HAL_UART_Transmit(&huart3, (uint8_t *)&rxData, 1, 100);
-
-	       // ตรวจสอบว่าเป็นตัวอักษร 'r' หรือไม่
-	       if(rxData == 'r')
-	       {
-	         // เพิ่มค่า duty cycle สำหรับ red channel ทีละ 20% (200 ถ้า ARR = 1000)
-	    	  R_DutyCycle += 20;
-	         if (R_DutyCycle > 100){
-	        	 R_DutyCycle = 0;
-	         }
-
-	         // อัพเดตค่า PWM สำหรับ TIM2_CH1 (Red LED)
-	         __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, (1000 - 1 )* ((float)R_DutyCycle/100));
-
-	       } else if (rxData == 'g') {
-	    	   G_DutyCycle += 20;
-			 if (G_DutyCycle > 100){
-				 G_DutyCycle = 0;
-			 }
-
-			 // อัพเดตค่า PWM สำหรับ TIM2_CH1 (Red LED)
-			 __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, (1000 - 1 )* ((float)G_DutyCycle/100));
-
-	       }else if (rxData == 'b') {
-	    	   B_DutyCycle += 20;
-			 if (B_DutyCycle > 100){
-				 B_DutyCycle = 0;
-			 }
-
-			 // อัพเดตค่า PWM สำหรับ TIM2_CH1 (Red LED)
-			 __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, (1000 - 1 )* ((float)B_DutyCycle/100));
-
-	       }
-	       memset(tx_message, 0, sizeof(tx_message));
-		  snprintf(tx_message, sizeof(tx_message), "\r\n(R %u, G %u, B %u)Button => ", R_DutyCycle, G_DutyCycle, B_DutyCycle);
-		  HAL_UART_Transmit(&huart3, (uint8_t*)tx_message, sizeof(tx_message), HAL_MAX_DELAY);
-	       // คุณสามารถเพิ่มคำสั่งอื่นๆ สำหรับการควบคุมช่องอื่น ๆ ได้ที่นี่
-	     }
+//	  if(HAL_UART_Receive(&huart3, &rxData, 1, 100) == HAL_OK)
+//	     {
+//		  HAL_UART_Transmit(&huart3, (uint8_t *)&rxData, 1, 100);
+//
+//	       // ตรวจสอบว่าเป็นตัวอักษร 'r' หรือไม่
+//	       if(rxData == 'r')
+//	       {
+//	         // เพิ่มค่า duty cycle สำหรับ red channel ทีละ 20% (200 ถ้า ARR = 1000)
+//	    	  R_DutyCycle += 20;
+//	         if (R_DutyCycle > 100){
+//	        	 R_DutyCycle = 0;
+//	         }
+//
+//	         // อัพเดตค่า PWM สำหรับ TIM2_CH1 (Red LED)
+//	         __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, (1000 - 1 )* ((float)R_DutyCycle/100));
+//
+//	       } else if (rxData == 'g') {
+//	    	   G_DutyCycle += 20;
+//			 if (G_DutyCycle > 100){
+//				 G_DutyCycle = 0;
+//			 }
+//
+//			 // อัพเดตค่า PWM สำหรับ TIM2_CH1 (Red LED)
+//			 __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, (1000 - 1 )* ((float)G_DutyCycle/100));
+//
+//	       }else if (rxData == 'b') {
+//	    	   B_DutyCycle += 20;
+//			 if (B_DutyCycle > 100){
+//				 B_DutyCycle = 0;
+//			 }
+//
+//			 // อัพเดตค่า PWM สำหรับ TIM2_CH1 (Red LED)
+//			 __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, (1000 - 1 )* ((float)B_DutyCycle/100));
+//
+//	       }
+//	       memset(tx_message, 0, sizeof(tx_message));
+//		  snprintf(tx_message, sizeof(tx_message), "\r\n(R %u, G %u, B %u)Button => ", R_DutyCycle, G_DutyCycle, B_DutyCycle);
+//		  HAL_UART_Transmit(&huart3, (uint8_t*)tx_message, sizeof(tx_message), HAL_MAX_DELAY);
+//	       // คุณสามารถเพิ่มคำสั่งอื่นๆ สำหรับการควบคุมช่องอื่น ๆ ได้ที่นี่
+//	     }
 	  // Set colors: Red (High), Green (Medium), Blue (Low)
 //	  memset(tx_message, 0, sizeof(tx_message));
 //	  snprintf(tx_message, sizeof(tx_message), "\r\nButton => ");
@@ -213,7 +212,7 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -221,21 +220,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 216;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
@@ -244,12 +230,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
     Error_Handler();
   }
@@ -275,9 +261,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 108-1;
+  htim2.Init.Prescaler = 1080-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1000-1;
+  htim2.Init.Period = 10000-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -300,7 +286,7 @@ static void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 1000/4-1;
+  sConfigOC.Pulse = 10000/4-1;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
